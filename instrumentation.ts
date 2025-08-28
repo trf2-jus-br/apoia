f3725d3b1fa091a82aa86c226e556b775d3e9404
Renato Crivano <renato.crivano@gmail.com>
2025-08-28 18:21:57 -0300

    refactor: move migrateIfNeeded import inside register function for better encapsulation

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,10 +1,7 @@
-import { migrateIfNeeded } from './lib/migrate-on-start'
-
 export async function register() {
-
-    await migrateIfNeeded()
-
     if (process.env.NEXT_RUNTIME === 'nodejs') {
+        const { migrateIfNeeded } = await import('./lib/migrate-on-start')
+        await migrateIfNeeded()
         if (!process.env.APP_CODE || !process.env.APP_HOST || !process.env.APP_PORT || !process.env.APP_REGISTRY_HOST || !process.env.APP_REGISTRY_PORT) return
         // await import('lib/eureka')
     }
