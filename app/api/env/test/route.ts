cd76e9a419a964f8509fcfe3b3a592e47d5ac389
Renato Crivano <renato.crivano@gmail.com>
2025-08-29 17:51:17 -0300

    feat: refactor GET endpoints to enhance user authentication and database accessibility checks

--- app/api/env/test/route.ts
+++ app/api/env/test/route.ts
@@ -1,28 +1,18 @@
+import { Dao } from "@/lib/db/mysql"
 import { CargaDeConteudoEnum, obterDadosDoProcesso2 } from "@/lib/proc/process"
 import { getCurrentUser } from "@/lib/user"
 import { paramsList } from "@/lib/utils/env"
 
 export async function GET(req: Request) {
-    const pUser = getCurrentUser()
-    const user = await pUser
-    if (!user) return Response.json({ errormsg: 'Unauthorized' }, { status: 401 })
-
-    const url = new URL(req.url)
-    const dadosDoProcesso = await obterDadosDoProcesso2({ numeroDoProcesso: '01015894820201000000', pUser, conteudoDasPecasSelecionadas: CargaDeConteudoEnum.NAO })
-
+    const systemId = await Dao.assertSystemId('PDPJ')
 
     const r: any = {
         pass: undefined,
         tests: [
             {
-                name: 'user',
-                descr: 'verifica se o usuário está autenticado corretamente',
-                pass: user ? true : 'USUÁRIO NÃO AUTORIZADO'
-            },
-            {
-                name: 'process',
-                descr: 'verifica se um processo pode ser obtido no DataLake e se ele contém peças além das peças públicas',
-                pass: dadosDoProcesso.errorMsg ? dadosDoProcesso.errorMsg : !!dadosDoProcesso.arrayDeDadosDoProcesso[0].pecas.find(p => p.descr === 'OUTRAS PEÇAS') ? true : 'PEÇAS NÃO LOCALIZADAS'
+                name: 'database',
+                descr: 'verifica se o banco de dados está acessível',
+                pass: !!systemId
             },
         ]
     }
---
+++ app/api/env/user-info/route.ts
@@ -0,0 +1,13 @@
+import { getSelectedModelParams } from "@/lib/ai/model-server"
+import { Dao } from "@/lib/db/mysql"
+import { CargaDeConteudoEnum, obterDadosDoProcesso2 } from "@/lib/proc/process"
+import { getCurrentUser } from "@/lib/user"
+import { paramsList } from "@/lib/utils/env"
+
+export async function GET(req: Request) {
+    const pUser = getCurrentUser()
+    const user = await pUser
+    if (!user) return Response.json({ errormsg: 'Unauthorized' }, { status: 401 })
+    delete user.image
+    return Response.json(user)
+}
